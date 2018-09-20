c5c3bb4e5215de79b765ac30e5ca82994e8e7b60
Tobias Suchy <tobias@appsoluts.de>
2018-09-20 21:11:37 +0200

    Added Rails5 support

--- lib/jquery-ui-bootstrap-rails.rb
+++ lib/jquery-ui-bootstrap-rails.rb
@@ -5,10 +5,10 @@ end
 
 
 case ::Rails.version.to_s
-when /^4/
-  require 'jquery-ui-bootstrap-rails/engine'
 when /^3\.[12]/
   require 'jquery-ui-bootstrap-rails/engine3'
 when /^3\.[0]/
   require 'jquery-ui-bootstrap-rails/railtie'
+else
+  require 'jquery-ui-bootstrap-rails/engine'
 end
